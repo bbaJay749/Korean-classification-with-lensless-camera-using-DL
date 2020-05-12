ccee20f008ae8351795c61fcde4ff88b38567e82
Jaeyong Park <33316717+bbaJay749@users.noreply.github.com>
2020-05-12 13:43:02 +0900

    Update Lensless_model_for_Hangul.py

--- Lensless_model_for_Hangul.py
+++ Lensless_model_for_Hangul.py
@@ -132,10 +132,34 @@ hist = model.fit_generator(
 
 model.save_weights('lensless_classificaiton_phd08_1000data_가~하_aug_both.h5')
 
-print(hist.history['loss'])
-print(hist.history['acc'])
-print(hist.history['val_loss'])
-print(hist.history['val_acc'])
+#print(hist.history['loss'])
+#print(hist.history['acc'])
+#print(hist.history['val_loss'])
+#print(hist.history['val_acc'])
+
+## summarize history for accuracy
+plt.plot(hist.history['acc'])
+plt.plot(hist.history['val_acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'validate'], loc='upper left')
+plt.show()
+## summarize history for loss
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'validate'], loc='upper left')
+plt.show()
+## summarize history for lr
+plt.plot(hist.history['lr'])
+plt.title('model learning rate')
+plt.ylabel('lr')
+plt.xlabel('epoch')
+plt.legend(['lr'], loc='upper left')
+plt.show()
 
 print("-- Evaluate --")
 scores = model.evaluate_generator(validation_generator, steps=860)
